f8a7cb3bc7c88a817590f350baf70dd045f007a4
Reza <mohammad.r.hemmati@gmail.com>
2022-11-30 16:42:17 +0330

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "MapsSDK",
-            targets: ["MapsSDK"]
+            targets: ["MapsSDK", "GoogleMapsUtils"]
         ),
     ],
     dependencies: [
